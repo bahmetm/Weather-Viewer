7c669d787a0671779a429994f03267cacb7ecf0a
bahmetm <bagmet.maxim@gmail.com>
2023-10-28 15:36:12 +0200

    feat: add servlets

---
+++ src/main/java/com/bahmet/weatherviewer/servlet/authentication/SignInServlet.java
@@ -0,0 +1,20 @@
+package com.bahmet.weatherviewer.servlet.authentication;
+
+import com.bahmet.weatherviewer.servlet.BaseServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SignInServlet extends BaseServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
---
+++ src/main/java/com/bahmet/weatherviewer/servlet/authentication/SignOutServlet.java
@@ -0,0 +1,15 @@
+package com.bahmet.weatherviewer.servlet.authentication;
+
+import com.bahmet.weatherviewer.servlet.BaseServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SignOutServlet extends BaseServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
