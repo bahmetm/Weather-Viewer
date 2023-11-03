c29f3daae4c164d23ce2130352cc23e18bd6f3e3
bahmetm <bagmet.maxim@gmail.com>
2023-11-03 12:25:03 +0100

    add SessionDAO

--- src/main/java/com/bahmet/weatherviewer/dao/SessionDAO.java
+++ src/main/java/com/bahmet/weatherviewer/dao/SessionDAO.java
@@ -5,6 +5,7 @@ import com.bahmet.weatherviewer.util.PersistenceUtil;
 import com.bahmet.weatherviewer.exception.DatabaseException;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Query;
 import java.time.LocalDateTime;
@@ -12,44 +13,67 @@ import java.util.Optional;
 import java.util.UUID;
 
 public class SessionDAO {
-    private final EntityManager entityManager = PersistenceUtil.getEntityManagerFactory().createEntityManager();
+    private final EntityManagerFactory entityManagerFactory = PersistenceUtil.getEntityManagerFactory();
 
     public Optional<Session> findById(UUID id) {
-        Session session = entityManager.find(Session.class, id);
-        return Optional.ofNullable(session);
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        try {
+            Session session = entityManager.find(Session.class, id);
+            return Optional.ofNullable(session);
+        } catch (Exception e) {
+            throw new DatabaseException("Database error.");
+        } finally {
+            entityManager.close();
+        }
     }
 
     public void save(Session session) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
         try {
             transaction.begin();
 
             entityManager.persist(session);
-            entityManager.flush();
+//            entityManager.flush();
 
             transaction.commit();
         } catch (Exception e) {
             transaction.rollback();
             throw new DatabaseException("Database error.");
+        } finally {
+            entityManager.close();
         }
     }
 
     public void delete(Session session) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
         try {
             transaction.begin();
 
+            if (!entityManager.contains(session)) {
+                // If it's not managed, then merge it into the current persistence context.
+                session = entityManager.merge(session);
+            }
+
             entityManager.remove(session);
-            entityManager.flush();
+//            entityManager.flush();
 
             transaction.commit();
         } catch (Exception e) {
+            e.printStackTrace();
             transaction.rollback();
             throw new DatabaseException("Database error.");
+        } finally {
+            entityManager.close();
         }
     }
 
-    public void deleteSessionsExpiredAtTime(LocalDateTime dateTime) {
+    public void deleteExpiredSessions() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        LocalDateTime dateTime = LocalDateTime.now();
+
         Query query = entityManager.createQuery("delete from Session s where s.expiresAt <= :dateTime");
         query.setParameter("dateTime", dateTime);
 
@@ -65,6 +89,8 @@ public class SessionDAO {
         } catch (Exception e) {
             transaction.rollback();
             throw new DatabaseException("Database error.");
+        } finally {
+            entityManager.close();
         }
     }
 }
