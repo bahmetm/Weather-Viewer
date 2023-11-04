33648b5ca568bd370c272ecbe2292f1281d1c308
bahmetm <bagmet.maxim@gmail.com>
2023-11-04 12:33:22 +0100

    add UnixTimestampDeserializer

---
+++ src/main/java/com/bahmet/weatherviewer/util/UnixTimestampDeserializer.java
@@ -0,0 +1,20 @@
+package com.bahmet.weatherviewer.util;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class UnixTimestampDeserializer extends JsonDeserializer<LocalDateTime> {
+    @Override
+    public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+        long timestampInSeconds = jsonParser.getValueAsLong();
+
+        return LocalDateTime.ofInstant(Instant.ofEpochSecond(timestampInSeconds), ZoneId.systemDefault());
+    }
+}
